77ea56f7fe9eff4008afe3514b062a85da326f93
alain <alainnambi@gmail.com>
2022-04-08 15:23:55 +0200

    convertir les formulaires en tableau

--- lib/pm_login_web/live/component/plus_modal_live.ex
+++ lib/pm_login_web/live/component/plus_modal_live.ex
@@ -44,168 +44,103 @@ defmodule PmLoginWeb.LiveComponent.PlusModalLive do
                 <!-- MY FORM -->
                 <div class="modal-body">
 
-                  <!-- FIRST ROW -->
-                    <div class="row">
-                      <div class="column">
-                          <label class="zoom-out"> Nom </label>
-                          <div class="zoom-out text-break"><%= @card.name %></div>
-                      </div>
+                <table class="table-tasks-mobile" style="font-size: 11px;">
+                  <thead>
+                    <tr>
+                      <th>Nom</th>
+                      <th>Attributeur</th>
+                      <th>Contributeur</th>
+                      <th>Statut</th>
+                      <th>Priorité</th>
+                      <th>Date de début</th>
+                      <th>Date de fin</th>
+                      <th>Durée estimée</th>
+                      <th>Progression</th>
+                      <th>Date d'échéance</th>
+                      <th>Description</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td data-label="Nom" style="word-wrap: anywhere;">
+                        <%= @card.name %>
+                      </td>
+                      <td data-label="Attributeur">
+                        <div style="display: inline-flex;">
+                          <img class="profile-pic-mini" style="width: 20px; height: 20px;"
+                               src={Routes.static_path(@socket, "/#{@card.task.attributor.profile_picture}")}/>
 
-                      <div class="column">
-                        <label class="zoom-out">Attributeur </label>
-                        <div>
-                          <img class="profile-pic-mini" src={Routes.static_path(@socket, "/#{@card.task.attributor.profile_picture}")} width="36"/>
+                          <div style="margin-top: 2px; margin-left: 5px;">
+                            <%= @card.task.attributor.username %>
+                          </div>
                         </div>
-                        <div class="zoom-out">
-                          <%= @card.task.attributor.username %>
-                        </div>
-                      </div>
+                      </td>
+                      <%= if @card.task.contributor != nil do %>
+                        <td data-label="Contributeur">
+                          <div style="display: inline-flex;">
+                            <img class="profile-pic-mini" style="width: 20px; height: 20px;"
+                                 src={Routes.static_path(@socket, "/#{@card.task.contributor.profile_picture}")}/>
 
-                      <div>
-                        <%= if @card.task.contributor != nil do  %>
-                          <div class="column">
-                            <label class="zoom-out">Contributeur</label>
-                            <div>
-                              <img class="profile-pic-mini" src={Routes.static_path(@socket, "/#{@card.task.contributor.profile_picture}")} width="36"/>
-                            </div>
-                            <div class="zoom-out">
+                            <div style="margin-top: 2px; margin-left: 5px;">
                               <%= @card.task.contributor.username %>
                             </div>
                           </div>
+                        </td>
+                      <% end %>
+                      <td data-label="Statut"> <%= @card.task.status.title %> </td>
+                      <td data-label="Priorité">
+                        <%= case @card.task.priority.title do %>
+                          <% "Faible" -> %> <div class="low__priority__point__table"> Faible </div>
+                          <% "Moyenne" -> %> <div class="avg__priority__point__table"> Moyenne </div>
+                          <% "Importante" -> %><div class="high__priority__point__table"> Importante </div>
+                          <% "Urgente" -> %><div class="urg__priority__point__table"> Urgente </div>
+                          <% _ -> %><div class="priority__point__table"></div>
                         <% end %>
-                      </div>
+                      </td>
+                      <td data-label="Date de début">
+                        <%= Utilities.letters_date_format(@card.task.date_start) %>
+                      </td>
+                      <td data-label="Date de fin">
+                        <%= if @card.task.date_end != nil do
+                          Utilities.letters_date_format(@card.task.date_end)
+                        else
+                          "En attente"
+                        end %>
+                      </td>
+                      <td data-label="Durée estimée"> <%= @card.task.estimated_duration %> heure </td>
+                      <td data-label="Durée effectuée"> <%= @card.task.performed_duration %> heure</td>
+                      <td data-label="Progression"> <%= @card.task.progression %> % </td>
+                      <td data-label="Date d'échéance">
+                        <%= Utilities.letters_date_format(@card.task.deadline) %>
+                      </td>
+                      <td data-label="Description" style="word-wrap: anywhere;">
+                        <%= @card.task.description %>
+                      </td>
+                    </tr>
+                  </tbody>
+                </table>
 
-                    </div>
+                <div class="zoom-out" style="margin-bottom: 6px; font-size: 11px">
+                  Nombre approximatif d'heures par jour ouvrable pour l'intervenant pour terminer cette tâche avant la date d'échéance:
+                </div>
 
-                        <div class="row" style="margin-top: 6px;">
+                <p class="column zoom-out task-working-hours" style="margin-bottom: 10px">
+                  <%= PmLogin.Monitoring.avg_working_hours(@card.task) %> heures
+                </p>
 
-                          <div class="column">
-                            <label class="zoom-out">Statut</label>
-                            <div class="zoom-out">
-                              <%= @card.task.status.title %>
-                            </div>
-                          </div>
+                <button class="button button-outline" type="button" phx-click="left-button-click" phx-target={"#modal-#{@id}"}>
+                  <div>
+                    <%= @left_button %>
+                  </div>
+                </button>
 
-                          <div class="column">
-                            <div class="column">
-                              <label class="zoom-out">Priorité</label>
-                            </div>
-                            <div class="column">
-                              <div class="zoom-out">
-                              <%= case @card.task.priority.title do %>
-                                <% "Faible" -> %>
-                                  <div class="task-small">
-                                    <%= @card.task.priority.title %>
-                                  </div>
-                                <% "Moyenne" -> %>
-                                  <div class="task-average">
-                                    <%= @card.task.priority.title %>
-                                  </div>
-                                <% "Importante" -> %>
-                                  <div class="task-important">
-                                    <%= @card.task.priority.title %>
-                                  </div>
-                                <% "Urgente" -> %>
-                                  <div class="task-urgent">
-                                    <%= @card.task.priority.title %>
-                                  </div>
-                                <% _ -> %>
-                                  <div>
-                                    <p> Priorité non reconnu </p>
-                                  </div>
-                              <% end %>
-                              </div>
-                            </div>
-                          </div>
-
-                        </div>
-                        <!-- END OF SECOND ROW -->
-
-                          <!-- THIRD ROW -->
-                            <div class="row" style="margin-top: 6px;">
-                              <div class="column">
-                                <label class="zoom-out">Date de début</label>
-                                <div class="zoom-out">
-                                  <%= Utilities.letters_date_format(@card.task.date_start) %>
-                                </div>
-                              </div>
-
-                              <div class="column">
-                                <label class="zoom-out">Date de fin</label>
-                                <div class="zoom-out">
-                                  <%= if @card.task.date_end != nil do
-                                    Utilities.letters_date_format(@card.task.date_end)
-                                  else
-                                    "En attente"
-                                  end %>
-                                </div>
-                              </div>
-
-                            </div>
-                          <!-- END OF THIRD ROW -->
-
-                            <!-- FOURTH ROW -->
-                              <div class="row" style="margin-top: 6px;">
-
-                                <div class="column">
-                                  <label class="zoom-out">Durée estimée</label>
-                                  <p class="zoom-out"> <%= @card.task.estimated_duration %> heure</p>
-                                </div>
-
-                                <div class="column">
-                                  <label class="zoom-out">Durée effectuée</label>
-                                  <p class="zoom-out"><%= @card.task.performed_duration%> heure</p>
-                                </div>
-
-                              </div>
-                            <!-- END OF FOURTH ROW -->
-
-                              <!-- FOURTH ROW -->
-                                <div class="row" style="margin-top: -17px;">
-
-                                  <div class="column">
-                                    <label class="zoom-out">Progression</label>
-                                    <div class="zoom-out"><%= @card.task.progression%> %</div>
-                                  </div>
-
-                                  <div class="column">
-                                    <label class="zoom-out">Date d'échéance</label>
-                                    <div class="zoom-out"><%= Utilities.letters_date_format(@card.task.deadline)%></div>
-                                  </div>
-                                </div>
-
-                                <div class="row" style="margin-top: 6px; margin-bottom: 6px">
-                                  <div class="column">
-                                    <label class="zoom-out">Description</label>
-                                    <div class="text-break zoom-out"> <%= @card.task.description %> </div>
-                                  </div>
-                                </div>
-                              <!-- END OF FOURTH ROW -->
-
-                              <!-- FIFTH ROW -->
-                              <div class="zoom-out" style="margin-bottom: 6px">
-                                Nombre approximatif d'heures par jour ouvrable pour l'intervenant pour terminer cette tâche avant la date d'échéance:
-                              </div>
-                              <p class="column zoom-out task-working-hours" style="margin-bottom: 10px"><%= PmLogin.Monitoring.avg_working_hours(@card.task) %> heures</p>
-
-
-
-                              <!-- END OF SIXTH ROW -->
-
-                    <!-- Buttons -->
-
-                      <button class="button button-outline" type="button" phx-click="left-button-click" phx-target={"#modal-#{@id}"}>
-                        <div>
-                          <%= @left_button %>
-                        </div>
-                      </button>
-
-                      <div class="row">
-                        <div class="column column-100">
-                          <i style="font-size: 10px;">Créee le <%= Utilities.simple_date_format_with_hours(@card.task.inserted_at) %>
-                          </i>
-                        </div>
-                      </div>
+                <div class="row">
+                  <div class="column column-100">
+                    <i style="font-size: 10px;">
+                      Créee le <%= Utilities.simple_date_format_with_hours(@card.task.inserted_at) %>
+                    </i>
+                  </div>
+                </div>
 
                 </div>
 
