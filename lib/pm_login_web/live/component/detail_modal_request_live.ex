2f5dc066dd35638601576cb28bf9ed7a8fac7903
alain <alainnambi@gmail.com>
2022-06-03 18:34:20 +0200

    mise en forme des demandes et du tableau de bord

--- lib/pm_login_web/live/component/detail_modal_request_live.ex
+++ lib/pm_login_web/live/component/detail_modal_request_live.ex
@@ -19,8 +19,11 @@ defmodule PmLoginWeb.LiveComponent.DetailModalRequestLive do
         id="task_modal_container"
         class="modal-container"
         style={
-                "visibility: #{ if @show_detail_request_modal, do: "visible", else: "hidden" };
-                 opacity: #{ if @show_detail_request_modal, do: "1 !important", else: "0" };"
+                "
+                  visibility: #{ if @show_detail_request_modal, do: "visible", else: "hidden" };
+                  opacity: #{ if @show_detail_request_modal, do: "1 !important", else: "0" };
+                  overflow: hidden;
+                 "
               }
       >
       <%= if not is_nil(@client_request) do %>
@@ -67,15 +70,44 @@ defmodule PmLoginWeb.LiveComponent.DetailModalRequestLive do
                         </div>
                       </td>
                       <td data-label="Date d'envoi"> <%= Utilities.letters_date_format_with_hours(@client_request.date_post) %> </td>
-                      <!--
-                      <td data-label="Fichiers">
-                      </td>
-                      -->
-                      <%= if @client_request.content == nil or @client_request.content == " " or @client_request.content == [] do %>
-                        <td data-label="Description" style="word-wrap: anywhere;">
-                            Aucune description
+
+                      <td data-label="Fichiers" style="min-height: 40px;">
+                        <div class="" style="display: flex; float: right;">
+                            <%= for url <- @client_request.file_urls do %>
+                              <%= if Path.extname(url) in [".jpg", ".jpeg", ".png"] do %>
+                                <a href={url} style="font-size: 25px; margin: 0 0 0 15px">
+                                  <i class="bi bi-file-earmark-image" title={Path.basename(url)} style="color: #2c3e50"></i>
+                                </a>
+                              <% else %>
+                                <a href={url} download style="font-size: 25px; margin: 0 0 0 15px">
+                                  <%= cond do %>
+                                    <% String.ends_with?(Path.basename(url), ".pdf")   -> %> <i class="bi bi-filetype-pdf" title={Path.basename(url)} style="color: #e13f2d;"></i>
+                                    <% String.ends_with?(Path.basename(url), ".txt")   -> %> <i class="bi bi-filetype-txt" title={Path.basename(url)} style="color: #7f8c8d;"></i>
+                                    <% String.ends_with?(Path.basename(url), ".odt")   -> %> <i class="bi bi-files" title={Path.basename(url)} style="color: #2c3e50;"></i>
+                                    <% String.ends_with?(Path.basename(url), ".ods")   -> %> <i class="bi bi-files" title={Path.basename(url)} style="color: #2c3e50;"></i>
+                                    <% String.ends_with?(Path.basename(url), ".odp ")  -> %> <i class="bi bi-files" title={Path.basename(url)} style="color: #2c3e50;"></i>
+                                    <% String.ends_with?(Path.basename(url), ".csv")   -> %> <i class="bi bi-filetype-csv" title={Path.basename(url)} style="color: #1da355;"></i>
+                                    <% String.ends_with?(Path.basename(url), ".xml")   -> %> <i class="bi bi-filetype-xml" title={Path.basename(url)} style="color: #f39c12;"></i>
+                                    <% String.ends_with?(Path.basename(url), ".xls ")  -> %> <i class="bi bi-filetype-xls" title={Path.basename(url)} style="color: #27ae60;"></i>
+                                    <% String.ends_with?(Path.basename(url), ".xlsx")  -> %> <i class="bi bi-filetype-xlsx" title={Path.basename(url)} style="color: #27ae60;"></i>
+                                    <% String.ends_with?(Path.basename(url), ".ppt")   -> %> <i class="bi bi-filetype-ppt" title={Path.basename(url)} style="color: #e67e22;"></i>
+                                    <% String.ends_with?(Path.basename(url), ".pptx ") -> %> <i class="bi bi-filetype-pptx" title={Path.basename(url)} style="color: #e67e22;"></i>
+                                    <% String.ends_with?(Path.basename(url), ".doc")   -> %> <i class="bi bi-filetype-doc" title={Path.basename(url)} style="color: #2980b9;"></i>
+                                    <% String.ends_with?(Path.basename(url), ".docx")  -> %> <i class="bi bi-filetype-docx" title={Path.basename(url)} style="color: #2980b9;"></i>
+                                    <% true -> %>
+                                  <% end %>
+                                </a>
+                                <br>
+                              <% end %>
+                            <% end %>
+                          </div>
                         </td>
-                      <% end %>
+
+                        <%= if @client_request.content == nil or @client_request.content == " " or @client_request.content == [] do %>
+                          <td data-label="Description" style="word-wrap: anywhere;">
+                              Aucune description
+                          </td>
+                        <% end %>
                     </tr>
                   </tbody>
                 </table>
