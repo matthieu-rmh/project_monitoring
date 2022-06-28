975ac55d8060881dafc0d36292c00b724f9aa460
alainnambi <alainnambi@gmail.com>
2022-06-28 16:55:22 +0300

    Relier notifications_type à la table notifications

--- lib/pm_login/services/notification.ex
+++ lib/pm_login/services/notification.ex
@@ -2,11 +2,15 @@ defmodule PmLogin.Services.Notification do
   use Ecto.Schema
   import Ecto.Changeset
 
+  alias PmLogin.Services.Type
+
   schema "notifications" do
     field :content, :string
     field :seen, :boolean, default: false
     field :sender_id, :id
     field :receiver_id, :id
+
+    belongs_to :notifications_type, Type
 
     timestamps()
   end
@@ -20,7 +24,7 @@ defmodule PmLogin.Services.Notification do
 
   def create_changeset(notification, attrs) do
     notification
-    |> cast(attrs, [:content, :seen, :sender_id, :receiver_id])
+    |> cast(attrs, [:content, :seen, :sender_id, :receiver_id, :notifications_type_id])
     |> validate_required([:content, :seen, :sender_id, :receiver_id])
     |> put_change(:seen, false)
   end
