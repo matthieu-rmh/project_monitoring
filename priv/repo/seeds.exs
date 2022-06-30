b1e3744fe4420b4a317ca61b0912d27d846cafec
alainnambi <alainnambi@gmail.com>
2022-06-30 18:12:18 +0300

    Important: Valeurs par défaut des types de notifications

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,54 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+
+alias PmLogin.Repo
+alias PmLogin.Services.Type
+
+type =
+  [
+    %Type{
+      id: 1,
+      type: "moved"
+    },
+
+    %Type{
+      id: 2,
+      type: "archived"
+    },
+
+    %Type{
+      id: 3,
+      type: "deleted"
+    },
+
+    %Type{
+      id: 4,
+      type: "achieved"
+    },
+
+    %Type{
+      id: 5,
+      type: "created"
+    },
+
+    %Type{
+      id: 6,
+      type: "assigned"
+    },
+
+    %Type{
+      id: 7,
+      type: "updated"
+    },
+
+    %Type{
+      id: 8,
+      type: "requested"
+    }
+  ]
+
+for t <- type do
+  Repo.insert!(t)
+end
