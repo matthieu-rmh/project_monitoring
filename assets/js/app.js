b0a38702bf98dd3f3f64ebadd751592b2bb41572
Alain Nambi <alainnambi@gmail.com>
2022-07-12 15:03:20 +0300

    Update: Hotwire Turbo has a built in progress bar delay set to 500ms by default

--- assets/js/app.js
+++ assets/js/app.js
@@ -286,10 +286,23 @@ let liveSocket = new LiveSocket("/live", Socket, {
 }
 );
 
+let topBarScheduled = undefined;
+
 // Show progress bar on live navigation and form submits
 topbar.config({ barColors: { 0: "#29d" }, shadowColor: "rgba(0, 0, 0, .3)" })
-window.addEventListener("phx:page-loading-start", info => topbar.show())
-window.addEventListener("phx:page-loading-stop", info => topbar.hide())
+
+window.addEventListener("phx:page-loading-start", () => {
+  if(!topBarScheduled) {
+    topBarScheduled = setTimeout(() => {
+      topbar.show()
+    }, 500)
+  }
+})
+window.addEventListener("phx:page-loading-stop", () => {
+  clearTimeout(topBarScheduled)
+  topBarScheduled = undefined
+  topbar.hide()
+})
 
 // connect if there are any LiveViews on the page
 liveSocket.connect()
