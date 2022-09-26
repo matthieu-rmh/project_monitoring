32d8b5284876264f9edbba255bc40726f36ec966
alain <alainnambi@gmail.com>
2022-09-26 13:45:29 +0300

    Add the name of the contributor if the request will be created as a task.

--- lib/pm_login/email.ex
+++ lib/pm_login/email.ex
@@ -2,8 +2,7 @@ defmodule PmLogin.Email do
   import Swoosh.Email
 
   alias PmLogin.Mailer
-  alias PmLogin.Services
-  alias PmLogin.Utilities
+  alias PmLogin.{Services, Utilities, Monitoring, Login}
 
   def send_state_of_client_request(send_to, request_id) do
     request = Services.get_clients_request!(request_id)
@@ -16,7 +15,17 @@ defmodule PmLogin.Email do
         request.seen and request.ongoing and not request.done and not request.finished ->
           type = if is_nil(request.task_id) and not is_nil(request.project_id), do: "Projet", else: "Tâche"
 
-          "a été mise en traitement en tant que #{type}, le #{Utilities.simple_date_format_with_hours request.date_ongoing}."
+          case type do
+            "Tâche" ->
+              contributor_id = Monitoring.get_task!(request.task_id).contributor_id
+
+              username = Login.get_username(contributor_id)
+
+              "a été mise en traitement en tant que #{type},et sera traité(e) par #{username}."
+
+            _ ->
+              "a été mise en traitement en tant que #{type}."
+          end
 
         request.seen and request.ongoing and request.done and not request.finished ->
           "a été accomplie, le #{Utilities.simple_date_format_with_hours request.date_done}."
@@ -28,11 +37,18 @@ defmodule PmLogin.Email do
           "n'a pas encore été vue"
       end
 
+    date_ongoing =
+      case request.date_ongoing do
+        nil -> ""
+        _ -> Utilities.simple_date_format_with_hours request.date_ongoing
+      end
+
     client = Services.get_active_client!(request.active_client_id)
 
     content = "
       <p> Bonjour #{client.user.username}, <br/> <p>
-      <p> Votre demande nommée #{request.title}, ayant l'identifiant N°#{request.uuid}, #{state} </p>
+      <p> Votre ayant l'identifiant N°#{request.uuid}, #{state} <br /></p>
+      <p> Le #{date_ongoing} </p>
     "
 
     new()
