22a1ad06a40f3b212bd12e17abac7c64e1b1a642
alain <alainnambi@gmail.com>
2022-09-29 19:05:57 +0300

    Updated survey request live       -   Show loading when socket is not been connected       -  Update socket       -  Search survey request

--- lib/pm_login_web/live/services/survey_request_live.ex
+++ lib/pm_login_web/live/services/survey_request_live.ex
@@ -9,23 +9,46 @@ defmodule PmLoginWeb.Services.SurveyRequestLive do
     Services.subscribe()
     Services.subscribe_to_request_topic()
 
+    #================================================#
+    # Show loading when socket is not been connected #
+    #================================================#
+    if not connected?(socket) do
+      Process.send_after(self(), :socket_not_connected, 0)
+    end
+
+    socket =
+      socket
+      |> assign(
+      request: nil,
+      date_begin: "",
+      date_end: "",
+      active_clients: Services.list_active_clients(),
+      loading: false,
+      search_text: nil,
+      requests: Services.list_requests(),
+      show_detail_request_modal: false,
+      client_request: nil,
+      show_modal: false,
+      service_id: nil,
+      curr_user_id: curr_user_id,
+      show_notif: false,
+      notifs: Services.list_my_notifications_with_limit(curr_user_id, 4)
+     )
+
     {:ok,
-     socket
-     |> assign(search_text: nil)
-     |> assign(
-       requests: Services.list_requests(),
-       show_detail_request_modal: false,
-       client_request: nil,
-       show_modal: false,
-       service_id: nil,
-       curr_user_id: curr_user_id,
-       show_notif: false,
-       notifs: Services.list_my_notifications_with_limit(curr_user_id, 4)
-     ), layout: {PmLoginWeb.LayoutView, "admin_layout_live.html"}}
+     socket,
+     layout: {PmLoginWeb.LayoutView, "admin_layout_live.html"}}
+  end
+
+  #================================================#
+  # Show loading when socket is not been connected #
+  #================================================#
+  def handle_info(:socket_not_connected, socket) do
+    {:noreply, socket |> assign(loading: true)}
   end
 
   #===========================#
-  # Switch Notification Event #
+  # Switch notification event #
   #===========================#
   def handle_event("switch-notif", %{}, socket) do
     notifs_length = socket.assigns.notifs |> length
@@ -53,7 +76,7 @@ defmodule PmLoginWeb.Services.SurveyRequestLive do
   end
 
   #============================#
-  # Loading Notification Event #
+  # Loading notification event #
   #============================#
   def handle_event("load-notifs", %{}, socket) do
     curr_user_id = socket.assigns.curr_user_id
@@ -66,11 +89,34 @@ defmodule PmLoginWeb.Services.SurveyRequestLive do
   end
 
   #===========================#
-  # Cancel Notification Event #
+  # Cancel notification event #
   #===========================#
   def handle_event("cancel-notif", %{}, socket) do
     cancel = if socket.assigns.show_notif, do: false
     {:noreply, socket |> assign(show_notif: cancel)}
+  end
+
+  #=======================#
+  # Search survey request #
+  #=======================#
+  def handle_event("search-survey-request", params, socket) do
+    date_begin = params["date_begin"]
+    date_end = params["date_end"]
+
+    request = Services.get_all_client_request_between_date(date_begin, date_end)
+
+    if not connected?(socket) do
+      Process.send_after(self(), :socket_not_connected, 0)
+    end
+
+    {:noreply,
+      socket
+      |> assign(
+        request: request,
+        date_begin: date_begin,
+        date_end: date_end
+      )
+    }
   end
 
   #====== Render ======#
