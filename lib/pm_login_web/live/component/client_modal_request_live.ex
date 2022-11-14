57c1814243559e489f27072e00547a431033a11d
Cleemzy <razafintsalama.rmh@gmai.com>
2022-11-14 14:47:53 +0300

    Tâche title

--- lib/pm_login_web/live/component/client_modal_request_live.ex
+++ lib/pm_login_web/live/component/client_modal_request_live.ex
@@ -112,7 +112,7 @@ defmodule PmLoginWeb.LiveComponent.ClientModalRequestLive do
                       <label class="btn btn-danger" for="success-outlined">Intervention majeure</label> -->
 
                       <%= radio_button f, :is_major, "true", class: "btn-check", id: "task_danger-outlined", autocomplete: "off" %>
-                      <%= label f, "danger-outlined", "INTERVENTION MAJEURE", class: "btn btn-danger", style: "font-size: 80%;"%>
+                      <%= label f, "danger-outlined", "TÂCHE MAJEURE", class: "btn btn-danger", style: "font-size: 80%;"%>
 
                       <%= radio_button f, :is_major, "false", class: "btn-check", id: "task_secondary-outlined", autocomplete: "off" , checked: true%>
                       <%= label f, "secondary-outlined", "Tâche mineure", class: "btn btn-outline-secondary", style: "font-size: 90%;" %>
